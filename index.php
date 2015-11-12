e6294e69e09f3cbeb94ecb62a5ce7a6d282b5491
mario oliveira <majroliv@gmail.com>
2015-11-12 13:06:39 -0200

    Iniciando login de usuario

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
-<?php include ('include/head.php') ?>
+<?php include ('include/head.php');?>
+
 <h1>Bem vindo!</h1>
+<h2>login</h2>
+
+
+<?php
+	if(isset($_GET["login"]) && $_GET["login"]==true) {
+?>
+	<p class="text-success">Logado com sucesso!</p>
+<?php 
+	}
+?>
+
+<?php
+	if(isset($_GET["login"]) && $_GET["login"]==false) {
+?>
+	<p class="text-danger">Usuário ou senha inválida!</p>
+<?php 
+	}
+?>
+
+<form action="login.php" method="post">
+	<table class="table">
+		<tr>
+			<td>Email</td>
+			<td><input type="email" name="email" class="form-control"></td>
+		</tr>
+		<tr>
+			<td>Senha</td>
+			<td><input type="password" name="senha" class="form-control"></td>
+		</tr>
+		<tr>
+			<td><button type="submit" class="btn btn-primary">Login</button></td>
+		</tr>
+	</table>
+</form>
 <?php include ('include/footer.php') ?>
---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+include ("include/conecta.php");
+include ("include/banco-usuario.php");
+
+$usuario = buscaUsuario($conexao, $_POST["email"], $_POST["senha"]);
+	if($usuario == null) {
+		header("location: index.php?login=0");
+	} else {
+		header("location: index.php?login=1");
+	}
+die;
